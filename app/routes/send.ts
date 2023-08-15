2a899dba2bdc225d13bfa01405d8dba1e7fc9ef1
Jayan Ratna <jayanratna@gmail.com>
2023-08-16 08:10:20 +1200

    chore: Update Node SDK usage (#2)

--- app/routes/send.ts
+++ app/routes/send.ts
@@ -5,9 +5,9 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const loader = async () => {
   try {
-    const data = await resend.sendEmail({
-      from: 'onboarding@resend.dev',
-      to: 'delivered@resend.dev',
+    const data = await resend.emails.send({
+      from: 'Acme <onboarding@resend.dev>',
+      to: ['delivered@resend.dev'],
       subject: 'Hello world',
       html: '<strong>It works!</strong>',
     });
